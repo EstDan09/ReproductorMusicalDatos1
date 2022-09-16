4bc5b6c1cd7d0c81fcd5dcaa3762edcb49c8139f
fabieds23 <fzamora@estudiantec.cr>
2022-09-16 11:18:31 -0600

    test 3.0

--- src/main/Users.java
+++ src/main/Users.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.io.Serializable;
 
 public class Users implements Serializable {
-    String nameUser;
+    String nameU;
     String email;
     String provi;
     String password;
