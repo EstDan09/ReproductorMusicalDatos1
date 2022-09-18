3878a07b745749e54ebf5543b25e96d3db9eec93
fabieds23 <fzamora@estudiantec.cr>
2022-09-18 13:41:03 -0600

    Edición del choice box en el createU fxml y creación de la clase de la música.

--- src/main/CreateUView.java
+++ src/main/CreateUView.java
@@ -1,4 +1,23 @@
 package main;
 
-public class CreateUView {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateUView implements Initializable{
+    @FXML
+    private ChoiceBox<String> provinciaBox;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        provinciaBox.getItems().add("San José");
+        provinciaBox.getItems().add("Alajuela");
+        provinciaBox.getItems().add("Cartago");
+        provinciaBox.getItems().add("Heredia");
+        provinciaBox.getItems().add("Puntarenas");
+        provinciaBox.getItems().add("Guanacaste");
+        provinciaBox.getItems().add("Limón");
+    }
 }
--- src/main/MainController.java
+++ src/main/MainController.java
@@ -6,22 +6,31 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class MainController {
+
     @FXML
-    private TextField textField1;
+    private TextField inicioCorreo;
+    @FXML
+    private TextField inicioContrasena;
+    @FXML
+    private Button iSesion;
     @FXML
     private Button crearCuenta;
 
+
     @FXML
-    private void saveUser() throws IOException {
-        String name = textField1.getText();
+    private void goToCrear() throws IOException {
+        String name = inicioCorreo.getText();
         System.out.println(name);
         FXMLLoader createUFxml = new FXMLLoader(getClass().getResource("createU-view.fxml"));
         Parent createUParent = createUFxml.load();
@@ -33,4 +42,10 @@ public class MainController {
         mainStage.close();
         createUStage.show();
     }
+
+    @FXML
+    private void goToMusic() throws IOException {
+    }
+
+
 }
